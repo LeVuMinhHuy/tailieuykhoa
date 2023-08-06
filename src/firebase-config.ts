9ca01040ea98031d236e8bde6c502cd703c7ebf6
Ted Vu <ted.vu@sourcecode.com.au>
2023-08-06 17:38:53 +0700

    [Init] CMS

---
+++ src/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCAoNfhx2Pmz6fvlHiURa6MHZxAy63ghF4",
+  authDomain: "reviewykhoa.firebaseapp.com",
+  projectId: "reviewykhoa",
+  storageBucket: "reviewykhoa.appspot.com",
+  messagingSenderId: "172516174324",
+  appId: "1:172516174324:web:84dda7d86e487f141b31a1",
+  measurementId: "G-CK2C3DXEB8",
+};
